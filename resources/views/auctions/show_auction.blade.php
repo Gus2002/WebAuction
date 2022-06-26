1957e0ceaa6827f8acc3b6ef1164c9292bbcce0b
Gus2002 <jaundzeikarsg@inbox.lv>
2022-06-26 15:51:18 +0300

    updated tailwindcss configuration

--- resources/views/auctions/show_auction.blade.php
+++ resources/views/auctions/show_auction.blade.php
@@ -1,8 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">Currently showing {{ $auction->id }} {{ $auction->name }} page</div>
-        <img src="{{ asset('images/' . $auction->image_path) }}" alt="">
+    <div class="flex justify-center mt-20">
+        <div class="mr-20 w-5/12 bg-white p-6 rounded-lg"> <img src="{{ asset('images/' . $auction->image_path) }}"
+                alt="">
+            <div class="mt-5 bg-white p-1 rounded-lg">
+                <p>{{ $auction->description }}</p>
+            </div>
+        </div>
+        <div class="flex w-3/12 bg-white p-6 rounded-lg justify-center">
+            <ul class="flex flex-col">
+                <h1 class="mb-10 font-bold text-2xl">{{ $auction->name }}</h1>
+                <li class="mb-10">
+                    <p>Type: {{ $auction->type }}</p>
+                </li>
+                <li class="mb-10">
+                    <p>Condition: {{ $auction->condition }}</p>
+                </li>
+                <li>
+                    <a href="" class="bg-blue-500 text-white px-4 py-3 rounded font-medium">Bid</a>
+                </li>
+            </ul>
+        </div>
     </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./resources/**/*.blade.php",
+     // "./resources/**/*.blade.php",
       "./resources/**/*.js",
       "./resources/**/*.vue",
+      "./resources/views/*.blade.php",
+      "./resources/views/auctions/*.blade.php",
+      "./resources/views/auth/*.blade.php",
+      "./resources/views/layouts/*.blade.php",
+      "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
     ],
     theme: {
       extend: {},
